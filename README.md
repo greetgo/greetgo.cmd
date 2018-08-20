6cab7b83bb27941d516fe0ceac7d095bf65485d5
ekolpakov <ekolpakov@greet-go.com>
2018-08-20 16:41:45 +0600

    Written install script

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # greetgo.cmd
 To install greetgo.cmd simple run in terminal following:
 
-        curl https://raw.githubusercontent.com/greetgo/greetgo.cmd/master/scripts/install.bash -sSf | bash
+    curl https://raw.githubusercontent.com/greetgo/greetgo.cmd/master/scripts/install.bash -sSf | bash
--- scripts/install.bash
+++ scripts/install.bash
@@ -1,3 +1,21 @@
 #!/usr/bin/env bash
 
-echo Hello World
+SHOW_TO_REBOOT=0
+
+if [ ! -d $HOME/bin/ ] ; then
+  SHOW_TO_REBOOT=1
+  mkdir $HOME/bin/
+fi
+
+curl https://raw.githubusercontent.com/greetgo/greetgo.cmd/master/scripts/greetgo.bash > $HOME/bin/gg
+
+chmod +x $HOME/bin/gg
+
+if [ "$SHOW_TO_REBOOT" = "1" ] ; then
+  echo "Installation complete. Please reboot the system and type gg in terminal"
+  exit 0
+fi
+
+echo "Installation complete."
+
+gg
